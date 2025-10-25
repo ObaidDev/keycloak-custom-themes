bd418288f0e046898c34a5601c723191422993f8
Abdellatif Hour <abdellatif.hour.web@gmail.com>
2025-10-25 23:47:34 +0100

    Init Glaobal theme

---
+++ a_gloabl_theme/src/components/ui/TrackSwiftlyLogo.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TrackSwiftlyLogo = () => {
+  return (
+    <div className="flex flex-col items-center">
+      {/* Text Logo #044eb0*/}
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-white tracking-tight">Track<span style={{ color: '#044eb0' }}>Swiftly</span></h1>
+        <p className="text-gray-200 mt-2">Real-time tracking and route optimization</p>
+      </div>
+    </div>
+  );
+};
+
+export default TrackSwiftlyLogo; 
